33cc7f51e6cd96525520295d7971a98e96417a01
KnotEnvy <jwsnyder@gmail.com>
2023-08-07 23:36:57 -0400

    Working version of quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,6 +6,7 @@ function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(null);
   const [score, setScore] = useState(0);
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
     axios.get(`/questions/${questionIndex}`).then(res => {
@@ -18,17 +19,28 @@ function Quiz() {
       if (res.data.isCorrect) {
         setScore(score + 1);
       }
+      if (questionIndex === 2) { // assuming there are 10 questions
+        setIsFinished(true);
+        return;
+      }
       setQuestionIndex(questionIndex + 1);
     });
   };
 
-  return currentQuestion ? (
-    <div>
-      <Question {...currentQuestion} onAnswer={handleAnswer} />
-      <p>Score: {score}</p>
-    </div>
+  return !isFinished ? (
+    currentQuestion ? (
+      <div>
+        <Question {...currentQuestion} onAnswer={handleAnswer} />
+        <p>Score: {score}</p>
+      </div>
+    ) : (
+      <p>Loading...</p>
+    )
   ) : (
-    <p>Loading...</p>
+    <div>
+      <h1>Congratulations, you are a Genius!</h1>
+      <p>Your final score is: {score} out of 10</p>
+    </div>
   );
 }
 
